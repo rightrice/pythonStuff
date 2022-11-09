4ee63f51d7a722409e7e20f1a38b6ceaa6bc1dbd
alan <rightrice@proton.me>
2022-11-09 13:17:27 -0500

    first

---
+++ fudge.py
@@ -0,0 +1,9 @@
+# watkins glen fudge co
+# building a robot fudge crafter
+##
+print("Hello, welcome to the watkins glen fudge company! My name is alan.")
+name0 = input("What is the name for your order? ")
+print("Hello " + name0 + ", what can i help you with today?")
+
+##
+
---
+++ learning.py
@@ -0,0 +1,11 @@
+#print("Hello Wrld")
+#print('i am just better')
+#print('large facts')
+#learning some python
+#
+print("""hello wrld
+juicey is
+better, just listen""")
+print("---")
+print("hello " + "wrld. \n" + "999 forever \n")
+print("\t   i am rightrice \n" * 3)
