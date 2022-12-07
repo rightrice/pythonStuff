b9ae7a7ff1fe2bba1315757d5a2bbc44dacbdaf2
rightrice <rightrice@proton.me>
2022-12-07 11:20:29 -0500

    The children were nestled all snug in their beds,

--- heyAlfred/ai.py
+++ heyAlfred/ai.py
@@ -5,7 +5,7 @@ import speech_recognition as sr
 
 
 class ai():
-    __name = ""
+    __name = "Alfred"
     __skill = []
 
     def __init__(self, name=None):
@@ -32,7 +32,7 @@ class ai():
         self.engine.runAndWait()
 
     def say(self, sentence):
-        self.engine.say(intro)
+        self.engine.say(sentence)
         self.engine.runAndWait()
 
     def listen(self):
@@ -40,13 +40,14 @@ class ai():
         with self.m as source:
             audio = self.r.listen(source)
         print("on it")
-    try:
-        phrase = self.r.recognize_google(audio, show_all=False, language="en")
-        senetence = "Just to confirm, you said" + phrase
-        self.engine.say(intro)
-        self.engine.runAndWait()
-    except e as error:
-        print("Sorry, I didn't hear you",e)
-        self.engine.say("Sorry, I misunderstood")
-        self.engine.runAndWait()
-    print("You said," + phrase)
+        try:
+            phrase = self.r.recognize_google(audio, show_all=False, language="en")
+            sentence = "Just to confirm, you said" + phrase
+            self.engine.say(sentence)
+            self.engine.runAndWait()
+        except e as error:
+            print("Sorry, I didn't hear you",e)
+            self.engine.say("Sorry, I misunderstood")
+            self.engine.runAndWait()
+        print("You said," + phrase)
+        return phrase
---
+++ heyAlfred/aiw.py
@@ -0,0 +1,59 @@
+## ai weather
+## created by rightrice
+
+from pyowm import OWM
+from geopy import Nominatim
+from datetime import datetime
+
+class weather():
+    #default location is Manhattan, NY USA
+    __location = "Manhattan, NY"
+    api_key = "0173afab2a3c6c4a054b81e8c675de60"
+
+    def __init__(self):
+        self.ow = OWM(self.api_key)
+        self.mgr = self.ow.weather_manager()
+        locator = Nominatim(user_agent="rightrice location")
+        city = "Manhattan"
+        country = "US"
+        self.location = city + ", " + country
+        loc = locator.geocode(self.__location)
+        self.lat = loc.latitude
+        self.long = loc.longitude
+    
+    @property
+    def weather(self):
+        forecast = self.mgr.one_call(lat=self.lat, long=self.long)
+        return forecast
+
+    def forecast(self):
+        forecast = self.mgr.one_call(lat=self.lat, long=self.long)
+        detail_status = forecast.forecast_hourly[0].detailed_status
+        pressure = forecast.forecast_hourly[0].pressure.get['press']
+        humidity = forecast.forecast_hourly[0].humidity
+        sunrise = datetime.utcfromtimestamp(forecast.forecast_hourly[0].sunrise()).strftime("%H:%M:%S")
+        sunset = datetime.utcfromtimestamp(forecast.forecast_hourly[0].sunset()).strftime("%H:%M:%S")
+        temperature = forecast.forecast_daily[0].tempurature('farenheit').get('day')
+        uvi = forecast.forecast_forecast[0].uvi
+
+        print('detailed status: ', detail_status)
+        print('pressure: ', pressure)
+        print('humidity: ', humidity)
+        print('tempurature: ', temperature)
+        print('sunrise: ', sunrise)
+        print('sunset: ', sunset)
+        print('uvi: ', uvi)
+        
+        message = "Today's weather forecast for Manhattan, NY USA." \
+                + "Today will be mostly " + detail_status + "." \
+                + "Pressure today will be " + pressure + "millibars." \
+                + "The humidity today is going to be " + humidity + "%." \
+                + "Today's tempurature will be " + temperature + "farenheit." \
+                + "The sunrise:" + sunrise + "AM EST." \
+                + "The sunset: " + sunset + "PM EST." \
+                + "The UV Index: " + uvi + ". This can be multiplied by 25 milliWatts per square meter."
+        return message
+
+## test
+rightrice = weather()
+print(rightrice.weather)
