e38bc6c65773d62ee17c77b6694f0fd9eeda5218
rightrice <rightrice@proton.me>
2022-12-11 12:19:42 -0500

    And then in a twinkling, I heard on the roof

---
+++ echoBot.py
@@ -0,0 +1,9 @@
+## echoBot
+## created by rightrice
+
+print("hello, welcome to the echoBot")
+print("the task of this bot is to echo what you say, back to you :), hence why it's called echo.")
+echo0 = (input("echoBot: what would you like me to echo? "))
+print("you said: " + echo0 + ".")
+echo1 = (input("anything else i should echo back? "))
+print("sure, this time you said: " + echo1)
