dcc0de202021f42d0e974b4b4d32d4b7b6be6e9f
rightrice <rightrice@proton.me>
2022-12-11 11:55:23 -0500

    So up to the house-top the coursers they flew,

---
+++ modulus.py
@@ -0,0 +1,11 @@
+## modulus equations
+## created by rightrice
+
+my_team = (27 % 4)
+print(my_team)
+
+neighbor0_team = (26 % 4)
+neighbor1_team = (28 % 4)
+
+print(neighbor0_team)
+print(neighbor1_team)
--- numbers.py
+++ numbers.py
@@ -14,3 +14,16 @@ math = 5 + 3 * 12 / 1.7
 results = age = actual_age + math
 
 print(results)
+
+## plus equals usage, creating variable updates
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price =+ (new_sneakers + nice_sweater + fun_books)
+print("The total price is", total_price)
