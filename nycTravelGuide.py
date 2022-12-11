9ceb0dd40ede1fbe90b5c19549108a81662c0939
rightrice <rightrice@proton.me>
2022-12-11 13:19:06 -0500

    The prancing and pawing of each little hoof.

---
+++ nycTravelGuide.py
@@ -0,0 +1,28 @@
+## nyc travel guide
+## created by rightrice, course by codecademy
+import random
+
+# travel directions
+def directions_to_timesSq():
+  print("Walk 4 mins to 34th St Herald Square train station.")
+  print("Take the Northbound N, Q, R, or W train 1 stop.")
+  print("Get off the Times Square 42nd Street stop.")
+  print("Take lots of pictures!")
+
+directions_to_timesSq()
+
+# travel weather
+## notes for future development:
+## would like to incorporate random to choose between 1 and 2 to determine if weather is good or bad
+print("Checking the weather for you!")
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
+weather_check()
+
+# travel expenses
+def calculate_expenses(plane_ticket_price, car_rental_rate, hotel_rate, trip_time):
+    car_rental_total = car_rental_rate * trip_time
+    hotel_total = hotel_rate * trip_time - 10
+    print(car_rental_total + hotel_total + plane_ticket_price)
+calculate_expenses(200,100,100,5)
