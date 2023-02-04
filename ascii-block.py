b5b90ed1acc3e295b3970dd075323f9766a79264
alan <rightrice@proton.me>
2023-02-03 20:44:57 -0500

    i'm back :)

---
+++ ascii-block.py
@@ -0,0 +1,29 @@
+## my name is rightrice
+## i like bitcoin
+name = """
+RRRRRRRR    RRRRRRRR
+R      R    R      R
+R     RR    R     RR
+R   RR      R   RR
+RR          RR
+R  R        R  R
+R    R      R    R
+R      R    R      R
+"""
+
+crypto = """
+
+
+    ₿  ₿
+  ₿₿₿₿₿₿₿₿
+  ₿       ₿
+  ₿       ₿
+  ₿₿₿₿₿₿₿₿
+  ₿       ₿
+  ₿       ₿
+  ₿₿₿₿₿₿₿₿
+    ₿  ₿
+"""
+
+print(name)
+print(crypto)
